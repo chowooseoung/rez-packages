698a1a4c8e68142ecceb59d73fbbc09766c9dd97
chowooseoung <chowooseoung@gmail.com>
2021-02-18 01:13:23 +0900

    customcomponent

--- int/rez-mgear_custom_component/0.0.1/package.py
+++ int/rez-mgear_custom_component/0.0.1/package.py
@@ -14,6 +14,25 @@ requires = [
     "mgear",
 ]
 
+_environ = {
+    "MGEAR_SHIFTER_COMPONENT_PATH": [
+        "{root}/payload/component"
+    ],
+    # "MGEAR_SHIFTER_CUSTOMSTEP_PATH": {
+    #     "{root}/python/mGear/build/custom_steps"
+    # },
+    # "MGEAR_SYNOPTIC_PATH": "{root}/python/mGear/env/synoptic",
+}
+
 def commands():
     env = globals()["env"]
-    env.MGEAR_SHIFTER_COMPONENT_PATH.append("{root}/payload/component")
+    this = globals()["this"]
+    expandvars = globals()["expandvars"]
+    
+    _environ = this._environ
+
+    for key, value in _environ.items():
+        if isinstance(value, (tuple, list)):
+            [env[key].append(expandvars(v)) for v in value]
+        else:
+            env[key] = expandvars(value)
